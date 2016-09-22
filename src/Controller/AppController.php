2004e0adc182529d4583138f5942d6509d260c7a
Tasuku Tozawa <15nm722x@vc.ibaraki.ac.jp>
2016-09-23 01:00:46 +0900

    ログイン機能を実装

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -43,6 +43,22 @@ class AppController extends Controller
 
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
+        $this->loadComponent('Auth', [
+            'authenticate' => [
+                'Form' => [
+                    'fields' => [
+                        'username' => 'id_string',
+                        'password' => 'password'
+                    ]
+                ]
+            ],
+            'loginAction' => [
+                'controller' => 'Users',
+                'action' => 'login'
+            ]
+        ]);
+
+        $this->Auth->allow(['display']);
     }
 
     /**
